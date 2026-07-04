d680368c63e5c3c4def8c75e1328bd913eb47d77
Azam <syedazam5656@gmail.com>
2026-07-04 13:08:38 +0530

    web app completed

---
+++ src/helper.py
@@ -0,0 +1,57 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from typing import List
+from langchain.schema import Document
+
+
+
+
+# Extract Data from the PDF File
+def load_pdf_file(data):
+    loader= DirectoryLoader(data,
+                            glob="*.pdf",
+                            loader_cls=PyPDFLoader)
+    
+
+    documents=loader.load()
+
+    return documents
+
+
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(
+          Document(  
+            page_content=doc.page_content,
+            metadata={"source": src}
+        )
+        )
+    return minimal_docs
+
+
+
+# Split the documents into smaller chunks
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,
+    )
+    texts_chunk = text_splitter.split_documents(extracted_data)
+    return texts_chunk
+
+    
+# Download the Embeddings from the Huggingface
+def download_embeddings():
+    """Download and return the HuggingFace Embeddings model."""
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name
+    )
+    return embeddings
+
+embedding = download_embeddings()
+
---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt = (
+    "You  are an Medical assisstant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "The question. If you don't know the answer, say you don't know. "
+    "Use three sentences maximum and keep the "
+    " answer concise. "
+    "\n\n"
+    "{context}"
+)
